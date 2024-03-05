a358c1d25629ec75483e9058a4400866956ded47
David Vinje <dnvinje@gmail.com>
2024-03-05 15:27:01 +0100

    first draft

--- Todo/App.js
+++ Todo/App.js
@@ -1,16 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Buy milk', key: 2},
+    { text: 'Climb with Faha', key: 1},
+    { text: 'Coffee with AK', key: 3},
+  ])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            keyExtractor={(item) => item.key}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
